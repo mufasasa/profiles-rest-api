5490475561ec0096fe38b1e295c763046b159857
Mohammed <mohammed.dahiru72@gmail.com>
2021-08-20 11:47:26 +0100

    enabled admin for profiles_api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -18,7 +18,7 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """ create and save a new super user with given details """
         user = self.create_user(email, name, password)
         user.is_superuser = True
