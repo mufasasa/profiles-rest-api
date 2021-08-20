6929a2418945c586a2416a701e034bf9498651e4
Mohammed <mohammed.dahiru72@gmail.com>
2021-08-20 23:47:04 +0100

    added viewsets in profiles API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
+from rest_framework import viewsets
 
 
 class helloAPIView(APIView):
@@ -44,3 +45,42 @@ class helloAPIView(APIView):
     def delete(self, request, pk=None):
         """ Handles deletion of an object """
         return Response({'method':'delete'})
+
+
+class helloViewSet(viewsets.ViewSet):
+    """Test API Viewset"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """ Return a hello message """
+
+        test = ['foo', 'bar', 'baz']
+        return Response({'message':'hello world', 'test':test})
+
+    def create(self, request):
+        """create a new object"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}!'
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST)
+
+        def retrieve(self, request, pk=None):
+            """Handles retrieving an object"""
+            return Response({'message':'HTTP Method GET'})
+
+        def update(self, request, pk=None):
+            """Handles updating an object"""
+            return Response({'HTTP Methon':'PUT'})
+
+        def patrial_update(self, request, pk=None):
+            """Handles updating part of an object"""
+            return Response({'HTTP Method':'PATCH'})
+
+        def destroy(self, request, pk=None):
+            """Handles deleting an object"""
+            return Response({'HTTP Method':'Delete'})
