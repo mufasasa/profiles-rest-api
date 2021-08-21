71a7a6ef2c14f8ad3ffe7fb0e3b605beac70f994
Mohammed <mohammed.dahiru72@gmail.com>
2021-08-21 20:10:35 +0100

    added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -9,6 +9,7 @@ router.register('profile', views.UserProfileViewSet),
 
 
 urlpatterns = [
-    path('hello-view', views.helloAPIView.as_view()),
-    path('', include(router.urls))
+    path('hello-view/', views.helloAPIView.as_view()),
+    path('', include(router.urls)),
+    path('login/', views.UserLoginApiView.as_view()),
 ]
